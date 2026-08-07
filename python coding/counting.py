42a4849ae381d52013382552beb2d970cff0142b
Benaniosam <benaniosam008@gmail.com>
2026-08-07 13:47:12 +0530

    did some coding exercises

---
+++ python coding/counting.py
@@ -0,0 +1,11 @@
+'''Counting and Finding
+
+Given the list numbers = [10, 25, 40, 25, 60, 25, 80]:
+
+Use .count() to find how many times 25 appears.
+
+Use .index() to locate the index position of 60.'''
+
+numbers = [10, 25, 40, 25, 60, 25, 80]
+print(numbers.count(25))
+print(numbers.index(60))
---
+++ python coding/remove_dup.py
@@ -0,0 +1,10 @@
+data = [3, 1, 2, 3, 4, 1, 5, 2]
+data.remove(3)
+data.pop(4)
+data.remove(2)
+data.sort()
+print (data)
+
+
+new =list(set(data))
+print(new)

f57e6b778118670128d6b33c63d70de0c2e9d532
Benaniosam <benaniosam008@gmail.com>
2026-08-10 14:25:20 +0530

    new changes

---
+++ Conditionals/nestedloop.py
@@ -0,0 +1,30 @@
+'''
+AND OPERATORS:
+
+Output is True only if both inputs are True.
+
+a      b        a AND b
+f      f            f
+f      t            f
+t      f            f 
+t      t            t  # True if both inputs are True
+
+OR OPERATORS:
+
+Output is True if at least one input is True.
+
+a      b        a OR b
+f      f           f  
+f      t           t 
+t      f           t 
+t      t           t   
+
+NOT OPERATORS:
+
+Inverts the input value.
+
+a  NOT  b
+f       t
+t       f 
+
+'''

eb66dc6a9ba135c4e1c475cf9424a3947045a594
Benaniosam <benaniosam008@gmail.com>
2026-08-11 11:31:01 +0530

    sa

--- Conditionals/condition.py
+++ Conditionals/condition.py
@@ -1,3 +1,4 @@
+'''
 score = 100
 if score < 0 or score > 100:
     print ("Invalid Mark")
@@ -21,3 +22,48 @@ elif user == 1:
     print("Single")
 else: 
     print("More")
+'''
+"""
+x = 0
+y = 5
+
+if x < y:
+    print ("Yes")
+
+if y:
+    print("you are right")
+
+if x or y:
+    print ("ya ya")
+
+if 'bena' in 'benaniosam':
+    print("True u are right")
+else:
+    print("Guess again")
+
+if 'sam' in ['ram','dom','boom','sam']:
+    print ("Yes he is here")
+else:
+    print("Nah your wrong")
+
+"""
+
+raining = True
+print("Let's go to the", 'beach' if not raining else 'library')
+
+age = 23
+s = 'minor' if age < 21 else 'adult'
+print(s)
+
+print('yes' if ('am' in ['ram','jam','sam']) else 'no')
+
+a = 1
+b = 2
+if a > b:
+    m = a
+else:
+    m = b
+print(m)
+# This can be written by one line:
+
+m = a if a > b else b
